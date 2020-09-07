cc47a7cc774e106b3d25717c8dda9160e70bad8d
coder-jason <69669843+coder-jason@users.noreply.github.com>
2020-09-07 17:45:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const name = 'Jason';
+const age = '30';
+
+console.log('Hi it\'s me ' + name + ' and my age is ' + age);
+
+console.log("Hi it's me " + name + " and my age is " + age);
+
+console.log(`Hi it's me ${name} and my age is ${age}`);
